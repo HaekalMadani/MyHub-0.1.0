20e027d4ca6bd9cb360e887ba15b6fc69af390bd
Haekal-Mac <haekal.madani@gmail.com>
2025-04-09 20:17:44 +0900

    redid most things

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./styles.css";
-import about from "./about.js";
+import about from "./dashboard.js";
+
+const tab1 = document.querySelector("#tab1");
+
+tab1.addEventListener("click", () => {
+    about();
+})
