f00fbe920747e94db5ac1b4bb78d2adcbc2a2ab1
Haekal Damar <haekal.madani@gmail.com>
2025-04-14 10:21:33 +0900

    .

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+// webpack.dev.js
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/template.html"],
+    hot: true,
+    open: true,
+  },
+});
