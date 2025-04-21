81dea8bdd9f7d93a7b3ae127799610c46713b29d
Haekal Damar <haekal.madani@gmail.com>
2025-04-21 11:06:04 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,53 +1,52 @@
 import "./styles.css";
-import "./schedule.css"
+import "./schedule.css";
 import "./dialog.css";
 import projects from "./project.js";
 import loadSchedule from "./timetable.js";
-import {showFlexTabs, showGridTab} from "./func.js";
+import { showFlexTabs, showGridTab } from "./func.js";
 
 // Default page (soon to be home page if u get up yo ass)
 showGridTab();
 projects();
 
 // DEV BUTTON ------------------------
-const dev = document.querySelector(".devbtn")
+const dev = document.querySelector(".devbtn");
 
-dev.addEventListener("click", () =>{
-    localStorage.removeItem('projects');
-    console.log("Local Storage Removed");  
-    location.reload(); 
-})
+dev.addEventListener("click", () => {
+  localStorage.removeItem("projects");
+  console.log("Local Storage Removed");
+  location.reload();
+});
 // -----------------------------------
 
-const tabs = document.querySelectorAll(".tab")
+const tabs = document.querySelectorAll(".tab");
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        if (tab.id === 'tab1'){ showGridTab();}
-        else{ showFlexTabs(); }
-    })
-})
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    if (tab.id === "tab1") {
+      showGridTab();
+    } else {
+      showFlexTabs();
+    }
+  });
+});
 
 // tab1 = projects
 const tab1 = document.querySelector("#tab1");
 
 tab1.addEventListener("click", () => {
-    const content = document.querySelector(".content");
-    content.innerHTML = "";
-    
+  const content = document.querySelector(".content");
+  content.innerHTML = "";
 
-    projects();
-})
+  projects();
+});
 
 // tab2 = schedule
 const tab2 = document.querySelector("#tab2");
 
 tab2.addEventListener("click", () => {
-    const content = document.querySelector(".content");
-    content.innerHTML = "";
+  const content = document.querySelector(".content");
+  content.innerHTML = "";
 
-    loadSchedule();
-})
-
-
-
+  loadSchedule();
+});
